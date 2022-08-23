e3bb766c605a6b1c51292c153df24be6090589d6
bagas1111 <111479891+bagas1111@users.noreply.github.com>
2022-08-22 17:35:33 -0700

    Add files via upload

---
+++ python4_bagas.py
@@ -0,0 +1,12 @@
+panjang = int(input("masukan nilai panjang: "))
+lebar =int(input("masukan nilai lebar:"))
+luas_pp = panjang * lebar 
+
+print("luas persegi panajang adalah: ", luas_pp)
+
+print("3. segitiga")
+ 
+alas = int(input("masukan nilai alas: "))
+tinggi = int(input("masukan nilai tinggi: "))
+luas_s = alas * tinggi / 2
+print("luas segitiga adalah", luas_s)
